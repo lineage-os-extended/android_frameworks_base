f4dac17e91e50c2968169597feb4f05a407bc759
Joey <joey@lineageos.org>
2018-06-14 11:11:35 +0200

    SystemUI: disable wallpaper-based tint for scrim
    This is quite annoying especially when the lockscreen is displaying an album art.
    You don't really want to see the green of your amazing wallpaper
    picturing a gorgerous forest landscape when you're rocking Master of Puppets.
    
    Also several users and contributors complained about this thing.
    Also it was removed in P dp1 and it never came back in later P builds.
    
    [BadDaemon]
    For dark text, use a fully transparent scrim so there is no negative impact
    for that usecase
    GlobalActionsDialog: Adapted and refactored code for less code change
    
    Change-Id: I5ce5d5ff002f3be9b13a4cbb8df2300d0d7dcb3b
    Signed-off-by: Joey <joey@lineageos.org>

--- packages/SystemUI/src/com/android/systemui/globalactions/GlobalActionsDialog.java
+++ packages/SystemUI/src/com/android/systemui/globalactions/GlobalActionsDialog.java
@@ -1,7 +1,7 @@
 /*
  * Copyright (C) 2017 The Android Open Source Project
  * Copyright (C) 2010-2015 CyanogenMod Project
- * Copyright (C) 2017-2018 The LineageOS Project
+ * Copyright (C) 2017-2019 The LineageOS Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file
  * except in compliance with the License. You may obtain a copy of the License at
@@ -42,6 +42,7 @@ import android.database.ContentObserver;
 import android.graphics.Bitmap;
 import android.graphics.BitmapShader;
 import android.graphics.Canvas;
+import android.graphics.Color;
 import android.graphics.Matrix;
 import android.graphics.Paint;
 import android.graphics.Point;
@@ -133,6 +134,9 @@ class GlobalActionsDialog implements DialogInterface.OnDismissListener,
     private static final String ACTION_ONEHAND_TRIGGER_EVENT =
             "com.android.server.wm.onehand.intent.action.ONEHAND_TRIGGER_EVENT";
 
+
+    // Default scrim color
+    private static final int SCRIM_DEFAULT_COLOR = Color.BLACK;
 
     private final Context mContext;
     private final GlobalActionsManager mWindowManagerFuncs;
@@ -1748,7 +1752,7 @@ class GlobalActionsDialog implements DialogInterface.OnDismissListener,
          * @param animate Interpolates gradient if true, just sets otherwise.
          */
         private void updateColors(GradientColors colors, boolean animate) {
-            mGradientDrawable.setColors(colors, animate);
+            mGradientDrawable.setColors(getDarkGradientColor(colors), animate);
             View decorView = getWindow().getDecorView();
             if (colors.supportsDarkText()) {
                 decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_NAVIGATION_BAR |
@@ -1756,6 +1760,14 @@ class GlobalActionsDialog implements DialogInterface.OnDismissListener,
             } else {
                 decorView.setSystemUiVisibility(0);
             }
+        }
+
+        private GradientColors getDarkGradientColor(GradientColors fromWallpaper) {
+            GradientColors colors = new GradientColors();
+            colors.setMainColor(SCRIM_DEFAULT_COLOR);
+            colors.setSecondaryColor(SCRIM_DEFAULT_COLOR);
+            colors.setSupportsDarkText(fromWallpaper.supportsDarkText());
+            return colors;
         }
 
         @Override
