9ee995a061a18375030f01c604624ea36c7b2f96
xyyx <xyyx@mail.ru>
2018-08-16 06:27:04 +0000

    base: Add Lockscreen Charging info (1/2)

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4143,6 +4143,15 @@ public final class Settings {
 
         /** @hide */
         public static final Validator SWIPE_TO_SCREENSHOT_VALIDATOR = BOOLEAN_VALIDATOR;
+		
+		 /**
+	         * Whether to show the battery info on the lockscreen while charging
+	         * @hide
+	         */
+	        public static final String LOCKSCREEN_BATTERY_INFO = "lockscreen_battery_info";
+	
+	        /** @hide */
+	        public static final Validator LOCKSCREEN_BATTERY_INFO_VALIDATOR = BOOLEAN_VALIDATOR;
       
         /**
          * Settings to backup. This is here so that it's in the same place as the settings
@@ -4319,6 +4328,7 @@ public final class Settings {
             PRIVATE_SETTINGS.add(SHOW_BATTERY_PERCENT);
             PRIVATE_SETTINGS.add(DISPLAY_COLOR_MODE);
 			PRIVATE_SETTINGS.add(SWIPE_TO_SCREENSHOT);
+			PRIVATE_SETTINGS.add(LOCKSCREEN_BATTERY_INFO);
         }
 
         /**
@@ -4410,7 +4420,8 @@ public final class Settings {
             VALIDATORS.put(NOTIFICATION_LIGHT_PULSE, BOOLEAN_VALIDATOR);
             VALIDATORS.put(ACCELEROMETER_ROTATION_ANGLES,
                     ACCELEROMETER_ROTATION_ANGLES_VALIDATOR);
-			VALIDATORS.put(SWIPE_TO_SCREENSHOT, SWIPE_TO_SCREENSHOT_VALIDATOR);		
+			VALIDATORS.put(SWIPE_TO_SCREENSHOT, SWIPE_TO_SCREENSHOT_VALIDATOR);	
+            VALIDATORS.put(LOCKSCREEN_BATTERY_INFO, LOCKSCREEN_BATTERY_INFO_VALIDATOR);			
         }
 
         /**
